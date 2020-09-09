95dbe2a7440d91e0a38a004db7908e71a8013173
gangelo <experator@outlook.fr>
2020-09-09 10:30:43 +0100

    Updates

---
+++ precache-manifest.7dc3eb5de89f79a46c68c0c65a08ddb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275a3e70cc9a28afe6cd913f484d67eb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b4ef1be61b11f4b3d444",
+    "url": "/monsters-rolodex/static/css/main.16ea15d2.chunk.css"
+  },
+  {
+    "revision": "f2cdafcaf7b267339659",
+    "url": "/monsters-rolodex/static/js/2.15e3d4d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.15e3d4d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ef1be61b11f4b3d444",
+    "url": "/monsters-rolodex/static/js/main.4d6c180d.chunk.js"
+  },
+  {
+    "revision": "22daa98acb52e89c127b",
+    "url": "/monsters-rolodex/static/js/runtime-main.45ac54f8.js"
+  }
+]);
